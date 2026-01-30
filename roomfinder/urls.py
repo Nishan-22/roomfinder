0c98869c5cb3f9c8737dc1b4c1613eee1ed9b578
Nishan-22 <paudelnishan47@gmail.com>
2026-01-30 17:06:56 +0545

    Add storage test endpoint

--- roomfinder/urls.py
+++ roomfinder/urls.py
@@ -21,10 +21,11 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
-
+from rooms.views import test_storage 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Rooms app
     path('', include('rooms.urls')),
+    path('test-storage/', test_storage),
 ]
